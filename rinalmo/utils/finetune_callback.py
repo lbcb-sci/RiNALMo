d01782d18e7845210928f03063e46003de6da571
RJPenic <rafael-josip.penic@fer.hr>
2024-03-04 22:30:37 +0000

    Finetune callback fix

--- rinalmo/utils/finetune_callback.py
+++ rinalmo/utils/finetune_callback.py
@@ -5,30 +5,12 @@ from torch.optim.optimizer import Optimizer
 import re
 import yaml
 
-def _collect_leaf_module_names(pl_module: LightningModule):
-    leaf_module_names = set()
+def _is_parent_module_unfrozen(module_name, potential_parent_modules):
+    for potential_parent_module_name in potential_parent_modules.keys():
+        if module_name.startswith(potential_parent_module_name):
+            return True
 
-    for module_name, _ in pl_module.named_modules():
-        # Ignore root module (module_name = '')
-        if not module_name:
-            continue
-
-        is_module_redundant = False
-
-        for leaf_module_name in set(leaf_module_names):
-            if module_name.startswith(leaf_module_name):
-                # Parent found, replace it with the child module
-                leaf_module_names.remove(leaf_module_name)
-                break
-            elif leaf_module_name.startswith(module_name):
-                # Child found, currently considered module is redundant
-                is_module_redundant = True
-                break
-
-        if not is_module_redundant:
-            leaf_module_names.add(module_name)
-
-    return leaf_module_names
+    return False
 
 class GradualUnfreezing(BaseFinetuning):
     def __init__(self, unfreeze_schedule_path: str):
@@ -42,40 +24,32 @@ class GradualUnfreezing(BaseFinetuning):
         for epoch in self.unfreeze_schedule:
             self.unfreeze_schedule[epoch] = re.compile('|'.join(self.unfreeze_schedule[epoch]))
 
-        self.leaf_module_names = None
-
     def freeze_before_training(self, pl_module: LightningModule) -> None:
-        modules_to_freeze = []
-        self.leaf_module_names = _collect_leaf_module_names(pl_module)
-
+        models_to_freeze = []
         for module_name, module in pl_module.named_modules():
-            # Skip non-leaf modules
-            if module_name not in self.leaf_module_names:
+            # Ignore root module (module_name = '')
+            if not module_name:
                 continue
 
-            # Collect all modules that are not tuned in the first (0-th) epoch
-            if not self.unfreeze_schedule[0].match(module_name):
-                modules_to_freeze.append(module)
+            # Collect all modules that are not tuned in the first epoch
+            if not bool(self.unfreeze_schedule[0].match(module_name)):
+                models_to_freeze.append(module)
 
         # Freeze collected modules
-        self.freeze(modules_to_freeze)
+        self.freeze(models_to_freeze)
 
     def finetune_function(self, pl_module: LightningModule, current_epoch: int, optimizer: Optimizer) -> None:
         if current_epoch in self.unfreeze_schedule and current_epoch != 0:
-            modules_to_unfreeze = []
+            modules_to_unfreeze = {}
 
+            # Collect next phase modules
             for module_name, module in pl_module.named_modules():
-                # Skip non-leaf modules
-                if module_name not in self.leaf_module_names:
-                    continue
-
-                # Collect modules to unfreeze
-                if self.unfreeze_schedule[current_epoch].match(module_name):
-                    modules_to_unfreeze.append(module)
+                if bool(self.unfreeze_schedule[current_epoch].match(module_name)) and not _is_parent_module_unfrozen(module_name, modules_to_unfreeze):
+                    modules_to_unfreeze[module_name] = module
 
             # Unfreeze collected modules
             self.unfreeze_and_add_param_group(
-                modules=modules_to_unfreeze,
+                modules=modules_to_unfreeze.values(),
                 optimizer=optimizer,
                 initial_denom_lr=1.0,
             )
